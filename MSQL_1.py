ee39010005d27c611fc321c0886da1e2366b50d2
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-06-18 22:54:23 -0300

    Comentário adicionado ao código.

--- MSQL_1.py
+++ MSQL_1.py
@@ -5,7 +5,7 @@ cursor = conexao.cursor() #entender a conexão
 
 insert_conta =('insert into investimento (empresa,data_invs,status_invs) values (%(empres_d)s,%(data_d)s,%(status_d)s)')
 
-dados = {'empres_d':'IPP',
+dados = {'empres_d':'IPP',                   #  Exemplo de informações para testar a ligação com o banco de dados.
          'data_d': 20200617,
          'status_d':'c'}
 cursor.execute (insert_conta,dados)
